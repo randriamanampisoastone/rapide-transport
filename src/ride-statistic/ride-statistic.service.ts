ad21419fa0a6477d7e1e6f80665ae5206f9c723e
Andriniaina <skyrazafimahaleo@gmail.com>
2025-02-25 11:05:27 +0300

    tsiresy

--- src/ride-statistic/ride-statistic.service.ts
+++ src/ride-statistic/ride-statistic.service.ts
@@ -131,4 +131,118 @@ export class RideStatisticService {
          throw error
       }
    }
+
+   async geGlobalRideStatistic() {
+      try {
+         const fiveDaysAgo = subDays(new Date(), 5)
+
+         // Exécuter toutes les requêtes Prisma en parallèle pour améliorer la performance
+         const [
+            totalRides,
+            allRides,
+            allRidesInLastFiveDays,
+            totalBalance,
+            allRidesInFiveDaysBalance,
+            ridesStatus,
+            ridesStatusLastFiveDays,
+            lastFiveDaysStatsNonFormated,
+         ] = await Promise.all([
+            // Nombre total de courses pour ce driver
+            this.prismaService.ride.count({}),
+
+            // Nombre total de courses pour chaque type de véhicule
+            this.prismaService.ride.groupBy({
+               by: ['vehicleType'],
+               _count: { vehicleType: true },
+            }),
+
+            // Nombre de courses effectuées ces 5 derniers jours
+            this.prismaService.ride.count({
+               where: {createdAt: { gte: fiveDaysAgo }},
+            }),
+
+            // Total de realPrice pour ce driver (uniquement si `status` est `COMPLETED`)
+            this.prismaService.ride.aggregate({
+               where: { status: 'COMPLETED' },
+               _sum: { realPrice: true },
+            }),
+
+            // Total de realPrice des 5 derniers jours pour chaque VehicleType (uniquement si `status` est `COMPLETED`)
+            this.prismaService.ride.groupBy({
+               by: ['vehicleType'],
+               where: {
+                  status: 'COMPLETED',
+                  createdAt: { gte: fiveDaysAgo },
+               },
+               _sum: { realPrice: true },
+            }),
+
+            // Nombre total de courses par status
+            this.prismaService.ride.groupBy({
+               by: ['status'],
+               _count: { status: true },
+            }),
+
+            // Nombre de courses par status sur les 5 derniers jours
+            this.prismaService.ride.groupBy({
+               by: ['status'],
+               where: {createdAt: { gte: fiveDaysAgo }},
+               _count: { status: true },
+            }),
+
+            this.prismaService.$queryRaw<
+               {
+                  date: Date
+                  status: string
+                  count: bigint
+                  totalBalance: bigint
+                  totalRides: bigint
+               }[]
+            >`SELECT DATE("createdAt") as "date", "status", COUNT(*) as "count", 
+               SUM(CASE WHEN "status" = 'COMPLETED' THEN "realPrice" ELSE 0 END) as "totalBalance", 
+               COUNT("rideId") as "totalRides" 
+               FROM "Ride" 
+               WHERE "createdAt" >= ${fiveDaysAgo} 
+               GROUP BY "date", "status" 
+               ORDER BY "date" ASC;`,
+         ])
+
+         const lastFiveDaysStats = lastFiveDaysStatsNonFormated.map((stat) => ({
+            date: stat.date,
+            status: stat.status,
+            count: Number(stat.count), // Convertir BigInt en Number
+            totalBalance: Number(stat.totalBalance),
+            totalRides: Number(stat.totalRides),
+         }))
+
+         return {
+            totalRides,
+            allRides: allRides.map(({ vehicleType, _count }) => ({
+               vehicleType,
+               count: _count.vehicleType,
+            })),
+            allRidesInLastFiveDays,
+            totalBalance: totalBalance._sum.realPrice || 0,
+            allRidesInFiveDaysBalance: allRidesInFiveDaysBalance.map(
+               ({ vehicleType, _sum }) => ({
+                  vehicleType,
+                  totalBalance: _sum.realPrice || 0,
+               }),
+            ),
+            ridesStatus: ridesStatus.map(({ status, _count }) => ({
+               status,
+               count: _count.status,
+            })),
+            ridesStatusLastFiveDays: ridesStatusLastFiveDays.map(
+               ({ status, _count }) => ({
+                  status,
+                  count: _count.status,
+               }),
+            ),
+            lastFiveDaysStats,
+         }
+      } catch (error) {
+         throw error
+      }
+   }
 }
