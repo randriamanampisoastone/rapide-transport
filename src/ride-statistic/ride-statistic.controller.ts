eb953eb0c32e0af346d2a4d9480e0dced3988976
Andriniaina <skyrazafimahaleo@gmail.com>
2025-02-25 10:32:31 +0300

    tsiresy

--- src/ride-statistic/ride-statistic.controller.ts
+++ src/ride-statistic/ride-statistic.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Query } from '@nestjs/common'
+import { Controller, Get, SetMetadata, UseGuards } from '@nestjs/common'
 import { RideStatisticService } from './ride-statistic.service'
+import { GetUser } from 'src/jwt/get.user.decorator'
+import { RolesGuard } from 'src/jwt/roles.guard'
 
 @Controller('ride-statistic')
 export class RideStatisticController {
    constructor(private readonly rideStatisticService: RideStatisticService) {}
 
    @Get('get-driver-statistic')
-   async getDriverStatistic(@Query('sub') driverProfileId: string) {
+   @SetMetadata('allowedRole', ['DRIVER'])
+   @UseGuards(RolesGuard)
+   async getDriverStatistic(@GetUser('sub') driverProfileId: string) {
       return await this.rideStatisticService.getDriverStatistic(driverProfileId)
    }
 }
