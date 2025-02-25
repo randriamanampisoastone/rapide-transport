8cc36e66545f3c6553368aa461e5621c6ed20a51
El Stone <ramdriamanampisoa.stone@gmail.com>
2025-02-25 23:41:53 +0300

    sdgvksdglkdf

--- prisma/migrations/20250208083040_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Profile" ALTER COLUMN "lastName" DROP NOT NULL;
--- src/ride/review-ride.service.ts
+++ src/ride/review-ride.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
-import { ReviewRideDto } from './dto/review-ride.dto'
-
 @Injectable()
 export class ReviewRideService {
    constructor(private readonly prismaService: PrismaService) {}
 
-   addRideReview(
+   async addRideReview(
       rideId: string,
       clientProfileId: string,
-      reviewRideDto: ReviewRideDto,
+      note: number,
+      review: string,
    ) {
       try {
-         const ride = this.prismaService.ride.update({
+         const ride = await this.prismaService.ride.update({
             where: {
                rideId,
                clientProfileId,
             },
             data: {
-               ...reviewRideDto,
+               note,
+               review,
             },
          })
          return ride
