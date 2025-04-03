d73c9c0898111e0fb7ccad2860c2c971ca91f54f
Harentsoa NOTOAVINA <harentsoa@newmanstech.com>
2025-04-03 22:07:20 +0300

    🎉 feat(orders): add OrderController and corresponding tests to manage order functionality.

--- src/shop/orders/orders.module.ts
+++ src/shop/orders/orders.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { OrderController } from './controller/order.controller';
 
-@Module({})
+@Module({
+  controllers: [OrderController]
+})
 export class OrdersModule {}
