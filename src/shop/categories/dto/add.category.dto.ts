3000b3215cb94e6726a2ac1f4fdbd92b9be92ff5
Harentsoa NOTOAVINA <harentsoa@newmanstech.com>
2025-04-05 09:21:34 +0300

    ✨ feat(dto): add Swagger API properties for category DTOs including name, description, type, and icon

--- src/shop/categories/dto/add.category.dto.ts
+++ src/shop/categories/dto/add.category.dto.ts
@@ -1,23 +1,29 @@
 import {IsEnum, IsNotEmpty, IsString} from "class-validator";
 import {Optional} from "@nestjs/common";
 import {CategoryTypeFor} from "../../../../enums/shop.enum";
+import {ApiProperty} from "@nestjs/swagger";
 
 
 export class AddCategoryDto {
+    @ApiProperty({ required: true, description: 'Category name', example: 'Exotic fruits' })
     @IsNotEmpty()
     @IsString()
     name: string
 
+    @ApiProperty({ required: false, description: 'Category description', example: 'Exotic fruits seen in paradisaical areas' })
     @Optional()
     @IsString()
     description?: string
 
+    @ApiProperty({ required: true, description: 'Category type', enum: CategoryTypeFor })
+    @IsNotEmpty()
     @IsEnum(CategoryTypeFor, {
         message:
             'For value mus ton of: MART, FOOD',
     })
     for: CategoryTypeFor
 
+    @ApiProperty({ required: true, description: 'Category icon', example: 'fas fa-apple' })
     @IsNotEmpty()
     @IsString()
     icon: string
--- src/shop/categories/dto/edit.category.dto.ts
+++ src/shop/categories/dto/edit.category.dto.ts
@@ -1,16 +1,20 @@
 import {IsEnum, IsOptional, IsString} from "class-validator";
 import {CategoryTypeFor} from "../../../../enums/shop.enum";
+import {ApiProperty} from "@nestjs/swagger";
 
 
 export class EditCategoryDto {
+    @ApiProperty({ required: false, description: 'Category name', example: 'Exotic fruits' })
     @IsOptional()
     @IsString()
     name?: string
 
+    @ApiProperty({ required: false, description: 'Category description', example: 'Exotic fruits seen in paradisaical areas' })
     @IsOptional()
     @IsString()
     description?: string
 
+    @ApiProperty({ required: false, description: 'Category type', enum: CategoryTypeFor })
     @IsOptional()
     @IsEnum(CategoryTypeFor, {
         message:
@@ -18,6 +22,7 @@ export class EditCategoryDto {
     })
     for?: CategoryTypeFor
 
+    @ApiProperty({ required: false, description: 'Category icon', example: 'fas fa-apple' })
     @IsOptional()
     @IsString()
     icon?: string
